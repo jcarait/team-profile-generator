c972c30f4d5b266e23992bef058e75a67fe1dcae
Jonathan F-Carait <jonathan.carait@outlook.com>
2022-02-16 21:05:39 +1100

    added node_modules to git ignore. Installed Jest. Working on employee.test

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,7 +1,16 @@
 // import Employee class
 
+const { hasUncaughtExceptionCaptureCallback } = require("process");
 const Employee = require("../lib/employeeClass");
 
+// creates employee object
 test("creates an object named Employee", () => {
-    const employee = new Employee ()
-})
+    const employee = new Employee("John", 123, "john.doe@mail.com");
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+//gets name from getName()
+test('')
