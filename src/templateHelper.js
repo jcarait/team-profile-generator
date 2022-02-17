4a0c3909fca45fea44ab91279215689344215b98
Jonathan F-Carait <jonathan.carait@outlook.com>
2022-02-17 15:53:45 +1100

    added templatehelper

--- src/templateHelper.js
+++ src/templateHelper.js
@@ -2,11 +2,11 @@
 
 const generateManagerCard = () =>
 
-`<div class="col-lg-4 col-md-6 mt-4">
+    `<div class="col-lg-4 col-md-6 mt-4">
 <div class="card manager-card" style="width: 18rem;">
     <div class="card-header">
-        <h2>Manager</h2>
-        <h3><span><i class="fa-solid fa-crown"></i> </span>Tywin Lannister</h3>
+        <h2>${manager.name}</h2>
+        <h3><span><i class="fa-solid fa-crown"></i> </span>Manager</h3>
     </div>
     <div class="card-body">
         <p class="rounded" id="id">ID: ${manager.id}</p>
@@ -18,25 +18,25 @@ const generateManagerCard = () =>
 
 // create engineer card
 
-const generateEngineerCard = () => 
+const generateEngineerCard = () =>
 
-`<div class="col-lg-4 col-md-6 mt-4">
+    `<div class="col-lg-4 col-md-6 mt-4">
 <div class="card manager-card" style="width: 18rem;">
     <div class="card-header">
-        <h2>Engineer</h2>
-        <h3><span><i class="fa-solid fa-chess-rook"></i> </span>Jamie Lannister</h3>
+        <h2>${engineer.name}</h2>
+        <h3><span><i class="fa-solid fa-chess-rook"></i> </span>Engineer</h3>
     </div>
     <div class="card-body">
-        <p class="rounded" id="id">ID: </p>
+        <p class="rounded" id="id">ID: ${engineer.id} </p>
         <p class="rounded" id="email">Email:<a href="mailto:${engineer.email}">${engineer.email}</a></p>
-        <p class="rounded" id="github">Github: </p>
+        <p class="rounded" id="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p></p>
     </div>
 </div>
 </div>`;
 
-const generateInterCard = () => 
+const generateInternCard = () =>
 
-`<div class="col-lg-4 col-md-6 mt-4">
+    `<div class="col-lg-4 col-md-6 mt-4">
 <div class="card intern-card" style="width: 18rem;">
     <div class="card-header">
         <h2>${intern.name}</h2>
@@ -49,3 +49,72 @@ const generateInterCard = () =>
     </div>
 </div>
 </div>`;
+
+const generateHTML = (data) => {
+
+    teamArray = [];
+
+    for (const employee of data) {
+
+        const role = employee.getRole();
+
+        switch (role) {
+            case 'Manager':
+                const managerCard = generateManagerCard(employee);
+                teamArray.push(managerCard)
+            break;
+            case 'Engineer':
+                const engineerCard = generateEngineerCard();
+                teamArray.push(engineerCard)
+            break;
+            case 'Intern':
+                const internCard = generateInternCard();
+                teamArray.push(internCard)
+            default:
+                console.log('No employee role found!')
+
+        }
+
+        const teamCards = teamArray.join("");
+
+        return generatePage(teamCards)
+    }
+}
+
+const generatePage = (card) => `
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Team Profile</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+        integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container d-flex justify-content-center">
+            <h1 class="title">My Team</h1>
+        </div>
+    </div>
+
+    <div class="container d-flex justify-content-center">
+        <div class="row">
+            ${card}
+            </row>
+
+
+        </div>
+        <script src="" async defer></script>
+</body>
+
+</html>
+`
